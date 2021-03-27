ed15cfab47c22dce5130677225352cebc8e5ba41
Hoàng Đại <daihoangnam3it@gmail.com>
2021-03-27 10:20:10 +0700

    make it better

--- fe/src/screens/ProductScreen.js
+++ fe/src/screens/ProductScreen.js
@@ -130,7 +130,7 @@ const ProductScreen = ({history, match }) => {
                   </ListGroup.Item>
                 ))}
                 <ListGroup.Item>
-                  <h2>Viết bình luận tại đây</h2>
+                  <h2>Viết bình luận tại đây nè</h2>
                   {successProductReview && (
                     <Message variant='success'>
                       Đánh giá thành công
