75feb84ec6d5159ba2d56eea928ecee6f2ebffe1
Hoàng Đại <daihoangnam3it@gmail.com>
2021-03-28 00:51:39 +0700

    make it better

--- be/controllers/productCtrl.js
+++ be/controllers/productCtrl.js
@@ -4,7 +4,7 @@ import User from '../models/userModel.js';
 const Product = {
   getAllProduct: async (req, res) => {
     try {
-      const pageSize = 6;
+      const pageSize = 8;
       const page = Number(req.query.pageNumber) || 1
       const keyword=req.query.keyword ? {
         name:{
